d1ff53e9a6e6e822750eb7cef3a5372338a712d2
Patricio Paulo <soladoporpatri@gmail.com>
2026-09-07 11:32:05 -0300

    Update GPU and System labels to Portuguese

---
+++ replace_again.py
@@ -0,0 +1,11 @@
+﻿import os
+
+html = open("index.html", "r", encoding="utf-8").read()
+
+html = html.replace('GPU<br/>TUNING', 'COMPUTADOR<br/>AJUSTADO')
+html = html.replace('SYSTEM<br/>OVERRIDE', 'SISTEMA<br/>OTIMIZADO')
+html = html.replace('GPU<br>TUNING', 'COMPUTADOR<br>AJUSTADO')
+html = html.replace('SYSTEM<br>OVERRIDE', 'SISTEMA<br>OTIMIZADO')
+
+with open("index.html", "w", encoding="utf-8") as f:
+    f.write(html)
